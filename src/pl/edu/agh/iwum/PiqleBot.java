f3ed0571c5ca2202f046bfadec35145f6ffecb27
kamilmielnik <kamil.adam.mielnik@gmail.com>
2015-06-04 21:02:57 +0200

    * bot can move away after hit by a bullet

--- src/pl/edu/agh/iwum/PiqleBot.java
+++ src/pl/edu/agh/iwum/PiqleBot.java
@@ -1,13 +1,18 @@
 package pl.edu.agh.iwum;
 
+import java.util.Random;
+
 import pl.edu.agh.iwum.actions.FireBullet;
 import pl.edu.agh.iwum.actions.GunTurn;
 import pl.edu.agh.iwum.actions.MoveAhead;
 import pl.edu.agh.iwum.actions.Turn;
+import robocode.HitByBulletEvent;
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
 
 public class PiqleBot extends Robot {
+
+	private Random random = new Random();
 
 	public void run() {
 		while (true) {
@@ -16,8 +21,15 @@ public class PiqleBot extends Robot {
 		}
 	}
 
-	public void onScannedRobot(ScannedRobotEvent e) {
-		attack(e);
+	@Override
+	public void onScannedRobot(ScannedRobotEvent event) {
+		attack(event);
+	}
+
+	@Override
+	public void onHitByBullet(HitByBulletEvent event) {
+		position90DegToBullet(event);
+		moveAway();
 	}
 
 	private void scanAround() {
@@ -31,6 +43,17 @@ public class PiqleBot extends Robot {
 
 	private void attack(ScannedRobotEvent e) {
 		FireBullet fireBullet = new FireBullet(3).execute(this);
+	}
+
+	private void position90DegToBullet(HitByBulletEvent event) {
+		double bulletHeading = event.getHeading();
+		double escapeAngle = bulletHeading + (random.nextBoolean() ? 90 : -90);
+		double angleToRotate = escapeAngle - this.getHeading();
+		Turn position90DegToBullet = new Turn(angleToRotate).execute(this);
+	}
+
+	private void moveAway() {
+		MoveAhead moveAhead = new MoveAhead(100).execute(this);
 	}
 
 }
